0c4fd0c3594777f563f3ac5625408f3f4a1e9c08
eth <jparkerpatton@gmail.com>
2023-02-19 15:03:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-from backend/marketdata import item_data, get_hashname
-import os
-import time
+from flask import Flask, request, jsonify
+from marketdata import item_data, get_hashname
 
-csmb = """                   __ 
- ____ ___  __ _   / / 
-/ __/(_-< /  ' \ / _ \ 
-\__//___//_/_/_//_.__/ 
-      \n══════════════════════"""
+app = Flask(__name__)
 
-print(csmb)
-
-mode = int(input("[1] Weapons\n[2] Cases\n[?] "))
-os.system('clear')
-
-if mode == 1:
-  print(csmb)
-  gun = input("[-] Gun\n[?] ")
-  os.system('clear');print(csmb)
-  skin = input("[-] Skin\n[?] ")
-  os.system('clear');print(csmb)
-  wear = int(input("[-] Wear\n┈┈┈┈┈┈┈┈┈┈┈┈┈\n[1] Factory New\n[2] Minimal Wear\n[3] Field Tested\n[4] Well Worn\n┈┈┈┈┈┈┈┈┈┈┈┈┈\n[?] "))
-  os.system('clear');print(csmb)
-  stat = 0
-  if input("[1] StatTrak\n[?] ") == '1':
-    stat = 1
-  start_time = time.time()
-  os.system('clear');print(csmb,"\nFetching data...")
+@app.route('/api/weapon', methods=['POST'])
+def get_item_data():
+  gun = request.json['gun']
+  skin = request.json['skin']
+  wear = request.json['wear']
+  stat = request.json['stat']
   hashname = get_hashname(gun, skin, wear, stat)
-  
-elif mode == 2:
-  print(csmb)
-  case = input("[-] Case\n[?] ")
-  start_time = time.time()
-  os.system('clear');print(csmb,"\nFetching data...")
+  try:
+      data = item_data(hashname)
+      return jsonify(data)
+  except:
+      return jsonify({"error": "Item data not available"})
+
+@app.route('/api/case', methods=['POST'])
+def get_case_data():
+  case = request.json['case']
   hashname = case.replace(' ', '%20')
-  
-try:
-  print(item_data(hashname))
-except:
-  exit("[!] Item data not avaliable")
-  
-print(f"Executed in {round(time.time() - start_time, 3)} seconds.")
+  try:
+      data = item_data(hashname)
+      return jsonify(data)
+  except:
+      return jsonify({"error": "Item data not available"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
